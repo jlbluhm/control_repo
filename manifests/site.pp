98268d3f09d8a7913f0ca2fd87bc5239a2b2617c
jlbluhm <43935130+jlbluhm@users.noreply.github.com>
2018-10-25 21:00:42 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -9,7 +9,7 @@ node 'master.puppet.vm' {
   include role::master_server
   file {'/root/README':
     ensure => file,
-    content => “Welcome to ${fqdn}"\n,
+    content => ${fqdn},
     owner => 'root',
   }
 }
